8ab1b5401b51f1a1d165823ca0d4f68843857fb4
irakm <kimirina45@hotmail.com>
2022-08-03 09:15:05 -0400

    second test

---
+++ HelloWorld/src/main/java/com/mycompany/helloworld/Hello.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.helloworld;
+
+/**
+ *@author Kim
+ *email: kimirina45@hotmail.com
+ *date: current date
+ *purpose: My first Hello World program in an IDE
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Irina!");
+    }
+}
